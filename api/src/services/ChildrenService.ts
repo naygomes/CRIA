0fad1527e732d6ed38aa30f39b691b157927e6bf
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-24 23:11:43 -0300

    feat: create findAll method children service

---
+++ api/src/services/ChildrenService.ts
@@ -0,0 +1,36 @@
+import {
+  IChild,
+  IChildrenRepository,
+  IFindAllParams,
+  IFindAllResponse,
+  IGetSummaryResponse,
+} from "@/types";
+import { isValidEmail } from "@/utils";
+
+export class ChildrenService {
+  constructor(private childrenRepository: IChildrenRepository) {}
+
+  async findAll(params: IFindAllParams = {}): Promise<IFindAllResponse | null> {
+    const isInvalidPage =
+      params.page !== undefined && (isNaN(params.page) || params.page < 1);
+    if (isInvalidPage) {
+      throw new Error(
+        "Ação inválida: O número da página deve ser um valor numérico maior que zero.",
+      );
+    }
+
+    const isInvalidLimit =
+      params.limit !== undefined && (isNaN(params.limit) || params.limit < 1);
+    if (isInvalidLimit) {
+      throw new Error(
+        "Ação inválida: O limite de itens deve ser um valor numérico maior que zero.",
+      );
+    }
+
+    const MAX_LIMIT = 50;
+    if (params.limit && params.limit > MAX_LIMIT) {
+      params.limit = MAX_LIMIT;
+    }
+    return this.childrenRepository.findAll(params);
+  }
+}
