45e3a0d58991e857cb82f180c39eedb491889208
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 03:08:46 -0300

    feat: create auth types file

---
+++ api/src/types/auth-repository.ts
@@ -0,0 +1,11 @@
+import { IUser } from "./index.js";
+
+export interface ILoginResponse {
+  token: string;
+  user: Omit<IUser, "password">;
+}
+
+export interface IAuthRepository {
+  findByEmail(email: string): Promise<IUser | null>;
+  findById(id: string): Promise<IUser | null>;
+}
--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -1,3 +1,5 @@
+export * from "./auth.js";
+export * from "./auth-repository.js";
 export * from "./child.js";
 export * from "./children-repository.js";
 export * from "./pagination.js";
