44e473fbf5682dea7dbff51bf9287f38d50a47a1
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 01:52:29 -0300

    feat: create findAll method on children controller

---
+++ api/src/controllers/ChildrenController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { ChildrenService } from "@/services";
+import { BadRequestError, NotFoundError } from "@/errors";
+import { IFindAllParams } from "@/types";
+
+export class ChildrenController {
+  constructor(private childrenService: ChildrenService) {}
+
+  findAll = async (req: Request, res: Response) => {
+    const { page, limit, neighborhood, wasReviewed, hasAlerts } = req.query;
+
+    if (
+      Array.isArray(page) ||
+      Array.isArray(limit) ||
+      Array.isArray(neighborhood) ||
+      Array.isArray(wasReviewed) ||
+      Array.isArray(hasAlerts)
+    ) {
+      throw new BadRequestError("Parâmetros duplicados não são permitidos.");
+    }
+
+    const params: IFindAllParams = {
+      page: typeof page === "string" ? Number(page as string) : undefined,
+      limit: typeof limit === "string" ? Number(limit as string) : undefined,
+      filters: {
+        neighborhood:
+          typeof neighborhood === "string" ? neighborhood : undefined,
+        wasReviewed:
+          wasReviewed !== undefined
+            ? (wasReviewed as string) === "true"
+            : undefined,
+        hasAlerts:
+          hasAlerts !== undefined
+            ? (hasAlerts as string) === "true"
+            : undefined,
+      },
+    };
+
+    const result = await this.childrenService.findAll(params);
+    res.json(result);
+  };
+}
