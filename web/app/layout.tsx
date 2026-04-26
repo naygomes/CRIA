10764090dcee6e44a143003dff51ca34a4e7c04e
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-26 04:19:03 -0300

    refactor: change project name and update layout structure

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Footer, Navbar } from "@/components";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -13,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vigia Menor",
+  title: "CRIA",
   icons: "/logo.png",
 };
 
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="pt-br">
+      <body className={`antialiased`}>
+        <div className="flex flex-col justify-between min-h-screen">
+          <div className="mt-3 flex flex-col justify-between gap-5">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
