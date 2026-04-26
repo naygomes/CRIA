e61363176ad70b832f261de03d6198e5100b0a5e
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-26 00:19:16 -0300

    feat: create useChildren hook

---
+++ web/hooks/index.ts
@@ -0,0 +1,3 @@
+export * from "./useAuth";
+export * from "./useChildren";
+export * from "./useFetch";
---
+++ web/hooks/useChildren.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import { useCallback, useMemo } from "react";
+import { useFetch, useAuth } from "@/hooks";
+import { CHILDREN_ROUTE } from "@/settings";
+import { IGetChildrenParams } from "@/types";
+
+export function useChildren() {
+  const { token } = useAuth();
+  const { get } = useFetch({ baseURL: CHILDREN_ROUTE });
+
+  const customHeaders = useMemo(
+    () => ({
+      Authorization: "Bearer " + token,
+    }),
+    [token],
+  );
+
+  const getChildren = useCallback(
+    async (params: IGetChildrenParams = {}) => {
+      if (!token) return;
+      try {
+        const response = await get({
+          endpoint: "",
+          params: {
+            ...params.filters,
+            page: params.page,
+            limit: params.limit,
+          },
+          customHeaders,
+        });
+        return response;
+      } catch (error) {
+        console.error("Erro ao buscar crianças:", error);
+      }
+    },
+    [get, customHeaders, token],
+  );
+
+  return { getChildren };
+}
