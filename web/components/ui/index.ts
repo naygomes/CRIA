f8f3438caab1ddfd74c281cdfadb14768a3b8eb6
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-27 18:19:33 -0300

    feat: create checkbox component

--- web/components/ui/index.ts
+++ web/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from "./alert";
 export * from "./badge";
 export * from "./button";
 export * from "./card";
+export * from "./checkbox";
 export * from "./input";
 export * from "./label";
 export * from "./native-select";
